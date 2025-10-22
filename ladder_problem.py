6c38b42462d8d4731d1853b526ed70a8f1ffe922
ZhdanovDmitriy <D.Zhdanov66@ya.ru>
2025-10-23 01:25:32 +0300

    added: rude_exp_coef and rude_compr_coef
    Effectiveness increased 10 times

--- ladder_problem.py
+++ ladder_problem.py
@@ -4,9 +4,11 @@ from matplotlib.animation import FuncAnimation
 
 a = 6.0
 b = 8.0
+Eps = 0.000001
 exp_coef = 1.001
+rude_exp_coef = 1.1
 compr_coef = 0.999
-Eps = 0.01
+rude_compr_coef = 0.9
 step_coef = 0.1
 
 if a > b: a, b = b, a
@@ -28,23 +30,18 @@ def find_point_C(A, B, L):
     return (x, y)
 
 fig, ax = plt.subplots()
-
 x_center = b
 y_center = -1.25 * a
 width = 3*b
 height = 4*a
-
 ax.set_xlim(x_center - width/2, x_center + width/2)
 ax.set_ylim(y_center - height/2, y_center + height/2)
-
 ax.set_aspect('equal')
-
 point_A, = ax.plot([], [], 'ro', label='A')
 point_B, = ax.plot([], [], 'bo', label='B')
 point_C, = ax.plot([], [], 'go', label='C')
 line_AC, = ax.plot([], [], 'k-', lw=1)
 line_AB, = ax.plot([], [], 'gray', lw=1)
-
 # y = 0, x > 0
 ax.plot([0, b + delta * max_iterations + 2], [0, 0], 'r-', lw=2)
 # y = -a, x > b
@@ -53,26 +50,31 @@ ax.plot([b, b + delta * max_iterations + 2], [-a, -a], 'r-', lw=2)
 ax.plot([0, 0], [-4 * a, 0], 'g-', lw=2)
 # x = b, y < -a
 ax.plot([b, b], [-4 * a - 3, -a], 'r-', lw=2)
-
 ax.legend(loc='upper right')
 
 
 cur_frame = 0
+rude_approx = True
 def update(frame):
-    global cur_frame, L, x_prev
+    global cur_frame, L, x_prev, delta, rude_approx
     cur_frame += 1
     A = (b + delta * cur_frame, 0)
     B = (b, -a)
     C = find_point_C(A, B, L)
 
 
-    if(x_prev - C[0] <= 0):
-        L = L * exp_coef
+    if(x_prev - C[0] < 0):
+        if(rude_approx):
+            L = L * rude_exp_coef
+        else:
+            L = L * exp_coef
         if(x_prev <= Eps):
             anim.event_source.stop()
+            ans = (a**(2/3) + b**(2/3))**(3/2)
             print("======================================================================================")
             print(f"Максимальная длина лестницы для a = {a}, b = {b} составляет {L}\nПотребовалось {frame} итераций")
-            print(f"Аналитический ответ: L = {(a**(2/3) + b**(2/3))**(3/2)}")
+            print(f"Аналитический ответ: L = {ans}")
+            print(f"Относительная погрешность: {abs(1 - L/ans) * 100} %")
             print("======================================================================================")
             return ()
         x_prev = b * 1.1
@@ -81,14 +83,20 @@ def update(frame):
         x_prev = C[0]
 
     if(C[0] < 0):
-        L = L * compr_coef
-        if(x_prev <= Eps):
-            anim.event_source.stop()
-            print("======================================================================================")
-            print(f"Максимальная длина лестницы для a = {a}, b = {b} составляет {L}\nПотребовалось {frame} итераций")
-            print(f"Аналитический ответ: L = {(a**(2/3) + b**(2/3))**(3/2)}")
-            print("======================================================================================")
-            return ()
+        if(rude_approx):
+            rude_approx = False
+            L = L * rude_compr_coef
+        else:
+            L = L * compr_coef
+            if(x_prev <= Eps):
+                anim.event_source.stop()
+                ans = (a**(2/3) + b**(2/3))**(3/2)
+                print("======================================================================================")
+                print(f"Максимальная длина лестницы для a = {a}, b = {b} составляет {L}\nПотребовалось {frame} итераций")
+                print(f"Аналитический ответ: L = {ans}")
+                print(f"Относительная погрешность: {abs(1 - L/ans) * 100} %")
+                print("======================================================================================")
+                return ()
         x_prev = b * 1.1
         cur_frame = 0
 
@@ -96,7 +104,6 @@ def update(frame):
     point_B.set_data([B[0]], [B[1]])
     point_C.set_data([C[0]], [C[1]])
 
-    line_AB.set_data([A[0], B[0]], [A[1], B[1]])
     line_AC.set_data([A[0], C[0]], [A[1], C[1]])
 
     info_text = ax.text(
